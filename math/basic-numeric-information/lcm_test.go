864cc744a4272f73d9aa37b8db2faaec5039887f
Anthony Olurin <anthony.olurin@metricaid.com>
2015-10-10 14:22:53 -0400

    LCM started, testing Travis

--- math/basic-numeric-information/lcm_test.go
+++ math/basic-numeric-information/lcm_test.go
@@ -1,1 +1,35 @@
 package spreadsheet
+
+import (
+	"errors"
+	"log"
+
+	"gopkg.in/go-playground/validator.v8"
+)
+
+// LCMstruct struct
+type LCMstruct struct {
+	Numbers []int `validate:"required"`
+}
+
+func validateLCM(nums []int) (*LCMstruct, error) {
+	validate := validator.New(&validator.Config{TagName: "validate"})
+
+	if len(nums) <= 0 {
+		return nil, errors.New("Number1, number2, ...    Number1 is required, subsequent numbers are optional. 1 to 255 values. If any value is not an integer, it is truncated.")
+	}
+
+	lcm := &LCMstruct{}
+
+	for _, number := range nums {
+		lcm.Numbers = append(lcm.Numbers, number)
+	}
+
+	errs := validate.Struct(lcm)
+	if errs != nil {
+		log.Println(errs)
+		return nil, errs
+	}
+
+	return lcm, nil
+}
