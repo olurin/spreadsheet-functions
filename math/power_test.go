a938a3630f9efbe031db7d2c1ef6fdb192446303
Anthony Olurin <anthony.olurin@metricaid.com>
2015-11-20 09:19:27 -0500

    Updates on Power Test Information

--- math/power_test.go
+++ math/power_test.go
@@ -5,17 +5,17 @@ import "testing"
 func TestPower(t *testing.T) {
 
 	p, err := Power(10, 4)
-	if p != 10000 && err != nil {
+	if p != 10000 || err != nil {
 		t.Errorf("Product Function Failed ... ")
 	}
 
 	p, err = Power(2, 4)
-	if p != 32 && err != nil {
+	if p != 32 || err != nil {
 		t.Errorf("Product Function Failed ... ")
 	}
 
 	p, err = Power(5, 2)
-	if p != 25 && err != nil {
+	if p != 25 || err != nil {
 		t.Errorf("Product Function Failed ... ")
 	}
 }
